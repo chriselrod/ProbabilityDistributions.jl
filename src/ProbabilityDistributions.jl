8cbb6ddbf40a20a7a8aca21766103fd0a5b63bf9
Chris Elrod <elrodc@gmail.com>
2019-03-21 13:16:46 -0500

    Working on adding a matrix normal distribution method for an autoregressive matrix and LKJ corr matrix.

--- src/ProbabilityDistributions.jl
+++ src/ProbabilityDistributions.jl
@@ -3,5 +3,6 @@ module ProbabilityDistributions
 using PaddedMatrices, SIMDPirates, SLEEFPirates, LoopVectorization
 
 include("distribution_functions.jl")
+include("normal_distribution.jl")
 
 end # module
