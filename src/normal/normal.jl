6015f015e8f61456c698ddef5f58714657f80d32
Chris Elrod <elrodc@gmail.com>
2019-10-31 07:32:24 -0400

    Minor progress; WIP.

--- src/normal/normal.jl
+++ src/normal/normal.jl
@@ -1,15 +1,19 @@
 
 
-struct StandardDeviation{T,P}
+struct StandardDeviation{T,P,L}
     σ::T
-    σ²::T
     σ⁻¹::T
     ∂σ∂i::P
+    logσ::L
+end
+struct StandardDeviationArray{S,I,L}
+    σ::S
+    σ⁻¹::I
+    logσ::L
 end
 
-
 @inline logdet_coef(Y, args...) = -Float64(size(Y, 1))
-@inline canonicalize_Σ(σ::T) where {T<:Real} = StandardDeviation{T,One}( σ, Base.FastMath.fast_mul(σ,σ), Base.FastMath.inv_fast(σ), One() )
+@inline canonicalize_Σ(σ::T) where {T<:Real} = StandardDeviation{T,One}( σ, Base.FastMath.inv_fast(σ), One(), log(σ) )
 @inline function canonicalize_Σ(σ::S) where {S <: Integer}
     T = promote_type(S, Float64)
     canonicalize_Σ(convert(T, σ))
@@ -20,9 +24,15 @@ end
     σ = Base.FastMath.sqrt_fast(σ²)
     σ⁻¹ = Base.FastMath.inv_fast(σ)
     ∂σ∂σ² = Base.FastMath.mul_fast(0.5, σ⁻¹)
-    StandardDeviatoin(
-        σ, σ², σ⁻¹, ∂σ∂σ²
+    StandardDeviation(
+        σ, σ⁻¹, ∂σ∂σ², log(σ)
     )
+end
+@inline function canonicalize_Σ(σ::RealFloat)
+    StandardDeviation( σ.r, Base.FastMath.inv_fast(σ.r), One(), log(σ) )
+end
+@inline function canonicalize_Σ(σ::AbstractFixedSizeArray)
+    StandardDeviation( PtrArray(σ), LazyMap(vinv, σ), LazyMap(SLEEFPirates.log, σ) )
 end
 
 """
